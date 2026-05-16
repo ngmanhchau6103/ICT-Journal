a616685abc45372fa55be605d5b343ac41e35250
chau <chau@chaus-MacBook-Air.local>
2026-05-16 17:47:58 +0700

    Add language setting

--- src/App.jsx
+++ src/App.jsx
@@ -3110,6 +3110,9 @@ function AuthScreen() {
 
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {
+        redirectTo: window.location.origin,
+      },
     });
 
     if (error) {
